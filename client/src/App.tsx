e7a289a882cc9b2f40e013e4090dce16dcfbf74a
sydeEvans <evan.wangjue@gmail.com>
2024-04-10 22:28:35 +0800

    修复问题

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -33,7 +33,6 @@ import {useEffect, useRef} from "react";
 import {client} from "./service/alist-client.ts";
 import {Button} from "@nextui-org/react";
 import {updateQueryParameter} from "./UpdateQueryParameter.tsx";
-import axios from "axios";
 
 function App() {
   const ref = useRef<MDXEditorMethods>(null);
